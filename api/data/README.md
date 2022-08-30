dbb60e4f50e4b3fdb91929a47f8a39fd22be2b59
Jeremy Zevin <jzevin@nbme.org>
2022-08-30 16:43:43 -0400

    add questions data

---
+++ api/data/README.md
@@ -0,0 +1,3 @@
+# API
+
+`DEBUG=api:* npm start`
--- api/routes/questions.js
+++ api/routes/questions.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var questions = require('../data/questions.json')
 
 /* GET questions listing. */
 router.get('/', function(req, res, next) {
-  res.send(['what', 'is', 'up', 'doc']);
+  res.send(questions.map(({id, question, options})=>{
+    return {id, question, options}
+  }));
 });
 
 module.exports = router;
